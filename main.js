74f3ff19f5cc7a57e96c53c768f0de14195d2d96
davegregg@gmail.com <davegregg@gmail.com>
2023-05-19 15:25:46 -0400

    In-class demo code.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const selectElement = document.querySelector("#album-choice")
+
 window.onload = main
 
 function main () {
@@ -6,8 +8,12 @@ function main () {
 }
 
 function populateAlbumDropdown() {
-    const selectElement = document.querySelector("#album-choice")
-    selectElement.onchange = loadImages
+
+    // The target of a form's "submit" event is the form.
+    // The target of a click event is the element clicked.
+    // The target of a change event is the element changed. 
+
+    selectElement.onchange = handleAlbumChange
 
     for (const album of albums) {
         // Example: <option value="album.folder">album.name</option>
@@ -24,17 +30,32 @@ function populateAlbumDropdown() {
     }
 }
 
-function loadImages (album) {
-    const parentElement = document.querySelector("main")
+function handleAlbumChange (event) {
+    // The "event" object describes the event that occurred.
+    // "event.target" is the element that is the target of the event. In other words, in the case of an "onchange" event, event.target is the element that changed ("<select>"). In the case of a "click" event, event.target is the element that was clicked.
 
-    for (const path of album.paths) {
+    console.log(event.target.value)
+    console.log(selectElement.value)
+    const selectedAlbumFolder = selectElement.value
+
+    // This function determines whether a given album is the one the user selected.
+    // When this function returns TRUE, the find() method will consider it's job done.
+    const albumFolderByUserChoice = album => album.folder === selectedAlbumFolder
+
+    // The find() method will search the "albums" array for an album which meets the condition set by albumFolderByUserChoice()
+    const selectedAlbum = albums.find(albumFolderByUserChoice)
+
+    const parentElement = document.querySelector("main")
+    parentElement.replaceChildren()  // parentElement.innerHTML = ""
+
+    for (const path of selectedAlbum.paths) {
         // VISUALIZE THE GOAL: <img class="card" src="insert-image-path-here">
 
         // 1. Create the empty element by tag name:
         const imageElement = document.createElement("img")
 
         // 2. Customize it in some way:
-        imageElement.setAttribute("src", path)
+        imageElement.setAttribute("src", `images/${selectedAlbumFolder}/${path}`)
         imageElement.classList.add("img-thumbnail", "gallery-image")
 
         // 3. Add it to an existing branch on the DOM tree:
@@ -43,5 +64,3 @@ function loadImages (album) {
 
     console.log(parentElement)
 }
-
-
